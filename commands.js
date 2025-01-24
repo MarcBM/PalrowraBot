8095c446c65d8122958d293d9283efa8ff2ac6d1
Marc Betbeder <marc.betbeder@hotmail.com>
2025-01-24 15:17:30 +1100

    Prepared for custom commands

--- commands.js
+++ commands.js
@@ -4,45 +4,37 @@ import { capitalize, InstallGlobalCommands } from './utils.js';
 
 // Get the game choices from game.js
 function createCommandChoices() {
-  const choices = getRPSChoices();
-  const commandChoices = [];
+	const choices = getRPSChoices();
+	const commandChoices = [];
 
-  for (let choice of choices) {
-    commandChoices.push({
-      name: capitalize(choice),
-      value: choice.toLowerCase(),
-    });
-  }
+	for (let choice of choices) {
+		commandChoices.push({
+			name: capitalize(choice),
+			value: choice.toLowerCase()
+		});
+	}
 
-  return commandChoices;
+	return commandChoices;
 }
 
 // Simple test command
 const TEST_COMMAND = {
-  name: 'test',
-  description: 'Basic command',
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 1, 2],
+	name: 'test',
+	description: 'Basic command',
+	type: 1,
+	integration_types: [0, 1],
+	contexts: [0, 1, 2]
 };
 
-// Command containing options
-const CHALLENGE_COMMAND = {
-  name: 'challenge',
-  description: 'Challenge to a match of rock paper scissors',
-  options: [
-    {
-      type: 3,
-      name: 'object',
-      description: 'Pick your object',
-      required: true,
-      choices: createCommandChoices(),
-    },
-  ],
-  type: 1,
-  integration_types: [0, 1],
-  contexts: [0, 2],
-};
+// Get my public IP address. (Append port)
+
+// Check if the server is online
+
+// Start the Server
+
+// Get List of Online Players
+
+// Shut the Server Down (make sure to wait 40s before doing so.)
 
 const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
 
